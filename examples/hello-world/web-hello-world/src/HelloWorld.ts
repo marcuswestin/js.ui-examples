e41273e34955013c1ab7f30eabf566dd9bd7d4b8
Marcus Westin <marcus.westin@gmail.com>
2020-11-27 19:14:03 -0500

    Use latest, with auto-inferred keys

--- examples/hello-world/web-hello-world/src/HelloWorld.ts
+++ examples/hello-world/web-hello-world/src/HelloWorld.ts
@@ -3,9 +3,18 @@ import { Col, Row, Style, TextStyle, TextView } from './lib/js.ui'
 export let HelloWorld = () => {
     return Row(Style({ height:'100%', alignItems:'center' }),
         Col(Style({ margin:'auto' }),
-            TextView(TextStyle({ color:'#456', fontSize:40, fontWeight:'200' }),
-                'Hello World!',
+            Row(
+                TextView(
+                    TextStyle({ color:'#456', fontSize:40, fontWeight:'200' }),
+                    'Hello World!',
+                ),
             ),
-        )
+            Row(
+                TextView(
+                    TextStyle({ color:'#456', fontSize:40, fontWeight:'200' }),
+                    'Hello World!',
+                ),
+            ),
+        ),
     )
 }
